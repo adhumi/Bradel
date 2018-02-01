ade556db3e5fa0f791077a8c528b5961e8b90ecb
Adrien Humiliere <adrien.humiliere@capitainetrain.com>
2018-02-01 20:34:02 +0100

    Initial Commit

---
+++ Bradel/Bradel.h
@@ -0,0 +1,19 @@
+//
+//  Bradel.h
+//  Bradel
+//
+//  Created by Adrien Humilière on 01/02/2018.
+//  Copyright © 2018 Adrien Humilière. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bradel.
+FOUNDATION_EXPORT double BradelVersionNumber;
+
+//! Project version string for Bradel.
+FOUNDATION_EXPORT const unsigned char BradelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bradel/PublicHeader.h>
+
+
