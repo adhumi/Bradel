bd3be07af4cf1a768f72defdc2fc24ffc13fbdc3
Adrien Humiliere <adrien.humiliere@brut.media>
2019-03-14 16:44:45 +0100

    Remove nil initialization of tableHeaderView/tableFooterView

--- Bradel/TableViewVC.swift
+++ Bradel/TableViewVC.swift
@@ -78,8 +78,6 @@ open class TableViewVC<VMType: TableViewVMProtocol>: UIViewController, UITableVi
         tableView.estimatedSectionHeaderHeight = tableView.style == .plain ? 22 : 10 // Default header height
         tableView.sectionFooterHeight = UITableView.automaticDimension
         tableView.estimatedSectionFooterHeight = tableView.style == .plain ? 22 : 10 // Default footer height
-        tableView.tableHeaderView = nil
-        tableView.tableFooterView = nil
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
 
